510b6cdf4f9af7e1b5ff75a6d857607197959811
Denizhan Gonen <gonendenizhan@gmail.com>
2025-07-16 16:31:20 +0300

    order-book system implemented in a tdd approach

--- order-book/orderBook.js
+++ order-book/orderBook.js
@@ -1,6 +1,17 @@
-class OrderBook {}
+class OrderBook {
+  constructor() {
+    this.orders = [];
+  }
+  addOrder(orderObj) {
+    this.orders.push(orderObj);
+  }
+  getOrders(type) {
+    return this.orders.filter((o) => o.type === type);
+  }
+  getOrder(id) {
+    return this.orders.find((o) => o.id === id) || null;
+  }
+}
 
-module.exports = {
-  OrderBook,
-};
+module.exports = { OrderBook };
 
--- order-book/orderBook.test.js
+++ order-book/orderBook.test.js
@@ -1,10 +1,48 @@
-/**
- * @jest-environment node
- */
+const { OrderBook } = require('./orderBook');
 
-/**
- * @typedef {import('@jest/globals')} jest
- */
+describe('Order Book', () => {
+  let orderBook;
 
-import { OrderBook } from './orderBook';
+  beforeEach(() => {
+    orderBook = new OrderBook();
+  });
+
+  test('should store and retrieve buy orders', () => {
+    const buyOrder1 = { id: '1', type: 'buy', price: 100, quantity: 5 };
+    const buyOrder2 = { id: '2', type: 'buy', price: 200, quantity: 15 };
+    const buyOrder3 = { id: '3', type: 'buy', price: 360, quantity: 25 };
+
+    orderBook.addOrder(buyOrder1);
+    orderBook.addOrder(buyOrder2);
+    orderBook.addOrder(buyOrder3);
+
+    expect(orderBook.getOrders('buy')).toEqual([
+      buyOrder1,
+      buyOrder2,
+      buyOrder3,
+    ]);
+  });
+  test('should store and retrieve sell orders', () => {
+    const order1 = { id: '1', type: 'sell', price: 100, quantity: 5 };
+    const order2 = { id: '2', type: 'sell', price: 200, quantity: 15 };
+
+    orderBook.addOrder(order1);
+    orderBook.addOrder(order2);
+
+    expect(orderBook.getOrders('sell')).toEqual([order1, order2]);
+  });
+  test('should return empty array if no orders of that type', () => {
+    expect(orderBook.getOrders('buy')).toEqual([]);
+  });
+  test('should return the order with given id', () => {
+    const order1 = { id: '1', type: 'sell', price: 100, quantity: 5 };
+
+    orderBook.addOrder(order1);
+
+    expect(orderBook.getOrder('1')).toEqual(order1);
+  });
+  test('should return null when no order found', () => {
+    expect(orderBook.getOrder('1')).toEqual(null);
+  });
+});
 
