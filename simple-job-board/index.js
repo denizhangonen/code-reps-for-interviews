21225456de3e74ab7acef5ef56182e2196945ef3
Denizhan Gonen <gonendenizhan@gmail.com>
2025-07-17 17:56:32 +0300

    prisma implemented and added some crud operations

--- simple-job-board/index.js
+++ simple-job-board/index.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
+const { PrismaClient } = require('@prisma/client');
 const { Pool } = require('pg');
+
+const prisma = new PrismaClient();
 
 const pool = new Pool({
   user: process.env.PGUSER,
@@ -9,9 +12,33 @@ const pool = new Pool({
   database: process.env.PGDATABASE,
 });
 
+const createData = async () => {
+  await prisma.job.create({
+    data: {
+      title: 'Full stack developer',
+      company: 'Collo AI',
+    },
+  });
+};
+
+const deleteData = async (id) => {
+  await prisma.job.delete({
+    where: {
+      id: id,
+    },
+  });
+};
+
 async function main() {
-  const result = await pool.query('SELECT * FROM USERS ');
-  console.log('Database time:', result.rows[0]);
+  //await createData();
+  // await deleteData(2);
+  // const result = await pool.query('SELECT * FROM USERS ');
+  // console.log('Database time:', result.rows[0]);
+  const users = await prisma.users.findMany();
+  const jobs = await prisma.job.findMany();
+
+  console.log('prisma - users:', users);
+  console.log('jobs:', jobs);
 }
 
 main();
