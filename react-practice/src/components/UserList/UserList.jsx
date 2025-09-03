4298792949248d453580580c5116c055a217faff
Denizhan Gonen <gonendenizhan@gmail.com>
2025-09-03 22:57:30 +0300

    useReducer implemented

--- react-practice/src/components/UserList/UserList.jsx
+++ react-practice/src/components/UserList/UserList.jsx
@@ -1,26 +1,69 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useReducer } from "react";
 
 const url = 'https://jsonplaceholder.typicode.com/users';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'set_users': {
+      return {
+        ...state,
+        users: action.payload
+      }
+    }
+    case 'loading_true': {
+      return {
+        ...state,
+        isLoading: true
+      }
+    }
+    case 'loading_false': {
+      return {
+        ...state,
+        isLoading: false
+      }
+    }
+    case 'set_error': {
+      return {
+        ...state,
+        errorMessage: action.payload
+      }
+    }
+    default: {
+      throw new Error('Unknown action:' + action.type)
+    }
+  }
+
+
+
+}
+
 const UserList = () => {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(false)
-  const [errorMessage, setErrorMessage] = useState(null)
+  const [state, dispatch] = useReducer(reducer, { users: [], isLoading: false, errorMessage: null })
+
+  const { users, isLoading, errorMessage } = state;
+
+  // const [users, setUsers] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false)
+  // const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     const fetchUsers = async () => {
-      setIsLoading(true)
+      // setIsLoading(true)
+      dispatch({ type: 'loading_true' })
       try {
         const response = await fetch(url);
         if (!response.ok) {
           throw new Error('response status:', response.status)
         }
         const result = await response.json();
-        setUsers(result);
+        // setUsers(result);
+        dispatch({ type: 'set_users', payload: result })
       } catch (e) {
-        setErrorMessage('Error loading users')
+        // setErrorMessage('Error loading users')
+        dispatch({ type: 'set_error', payload: 'Error loading users' })
       } finally {
-        setIsLoading(false)
+        // setIsLoading(false)
+        dispatch({ type: 'loading_false' })
       }
     }
     fetchUsers();
